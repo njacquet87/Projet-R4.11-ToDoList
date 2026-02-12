f90238879916123a7f733e31384c3e75c098640c
Noé Jacquet <noe.jacquet87@gmail.com>
2026-02-12 17:13:11 +0100

    add home page

--- ToDoProject/app/src/main/java/com/example/todoproject/MainActivity.kt
+++ ToDoProject/app/src/main/java/com/example/todoproject/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -25,8 +30,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -63,8 +71,12 @@ fun AppNavigation() {
             LogScreen(navController)
         }
 
-        composable(route = HOME) {
-            HomeScreen(navController)
+        composable(route = "$HOME/{name}/{firstName}") {
+            // get the arguments
+            backStackEntry -> val name = backStackEntry.arguments?.getString("name") ?: ""
+            val firstName = backStackEntry.arguments?.getString("firstName") ?: ""
+
+            HomeScreen(navController, name, firstName)
         }
     }
 }
@@ -83,6 +95,8 @@ fun LogScreen(navController: NavController) {
         Text(text = "Entrer votre nom et votre prenom",
             style = MaterialTheme.typography.bodySmall)
 
+        Spacer(modifier = Modifier.height(16.dp))
+
         Text(text = "Nom",
             style = MaterialTheme.typography.labelLarge)
 
@@ -98,6 +112,8 @@ fun LogScreen(navController: NavController) {
         Text(text = "Prenom",
             style = MaterialTheme.typography.labelLarge)
 
+        Spacer(modifier = Modifier.height(16.dp))
+
         var firstName by remember { mutableStateOf("") }
 
         // get name
@@ -106,13 +122,56 @@ fun LogScreen(navController: NavController) {
             label = {Text("Entrer votre prenom")},
             modifier = Modifier.fillMaxWidth().padding(16.dp))
 
-        OutlinedButton(onClick = {navController.navigate(HOME)}) {
-            Text(text = "Valider")
+        OutlinedButton(onClick = {
+            if (name.isNotEmpty() && firstName.isNotEmpty()) {
+                navController.navigate("$HOME/$name/$firstName")
+            }
+        }) {
+            Text(text = "Valider",
+                color = Color.Black)
         }
     }
 }
 
 @Composable
-fun HomeScreen(navController: NavController) {
+fun HomeScreen(navController: NavController, name: String, firstName: String) {
 
+    //header
+    Row(Modifier.background(Color.LightGray).fillMaxWidth().height(100.dp).padding(10.dp, 10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween) {
+
+        Text(text = APP_TITLE,
+            fontSize = 30.sp)
+
+        Text(text = "Bienvenue $name $firstName",
+            style = MaterialTheme.typography.bodySmall)
+    }
+
+    // body
+    Column(Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+
+        // add task
+        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Start) {
+            Text(text = "+",
+                fontSize = 15.sp,
+                modifier = Modifier.border(border = BorderStroke(2.dp, Color.Black),
+                    shape = RoundedCornerShape(5.dp)).padding(10.dp, 5.dp))
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text(text = "Ajouter une tache", style = MaterialTheme.typography.bodyMedium)
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // list of tasks
+        Column(Modifier.width(250.dp).height(500.dp)
+            .border(0.dp, Color.Gray, RoundedCornerShape(14.dp))
+            .clip(RoundedCornerShape(14.dp))
+            .background(Color.Gray)) {
+
+        }
+    }
 }
